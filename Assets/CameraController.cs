c5c5cbeaa75187bf980716a06ccc23213d75645c
ShawLevin <shawlevin@gmail.com>
2016-11-05 16:53:30 -0400

    initial setup of the unity with camera and sound
    initial setup of the unity with camera and sound

---
+++ Assets/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public WebCamTexture mCamera = null;
+    public GameObject plane;
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        Debug.Log("Script has been started");
+        plane = GameObject.FindWithTag("Player");
+
+        mCamera = new WebCamTexture();
+        plane.GetComponent<Renderer>().material.mainTexture = mCamera;
+        mCamera.Play();
+
+        if (mCamera.isPlaying)
+        {
+            Debug.Log("Camera is playing.");
+            Color[] pixels = mCamera.GetPixels(1, 1, 1, 1);
+            
+            Debug.Log(pixels.Length);
+        }
+        else
+        {
+            Debug.Log("Camera was told to play but is not playing!");
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DateTime.Now.Second % 5 == 0)
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+        }
+    }
+}
